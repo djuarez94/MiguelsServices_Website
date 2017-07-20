d42acdd3a920ea71c1b71d52a498d3f92de4b3a5
David Juarez <davidjuarez1411@gmail.com>
2017-07-19 23:43:08 -0700

    added png files for the carousel. Added jquery for animations on elements

--- index.php
+++ index.php
@@ -24,42 +24,42 @@
 			   </div>
 			   <div class="collapse navbar-collapse" id="myNavbar">
 				 <ul class="nav navbar-nav">
-				   <li><a href="#myCarousel">Home</a></li>
-				   <li><a href="#about">About</a></li>
-				   <li><a href="#services">Services</a></li>
-				   <li><a href="#contact">Contact</a></li>
+				   <li class="home"><a href="#myCarousel">Home</a></li>
+				   <li class="about"><a href="#about">About</a></li>
+				   <li class="services"><a href="#services">Services</a></li>
+				   <li class="contact2"><a href="#contact">Contact</a></li>
 				 </ul>
 			   </div>
 			 </div>
 		</nav>
-		<div id="myCarousel" class="carousel slide" data-ride="carousel" style="height: 500px;">
+		<div id="myCarousel" class="carousel slide home2" data-ride="carousel" style="height: 500px;">
 			<!-- Indicators -->
 			<ol class="carousel-indicators">
 				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
 				<li data-target="#myCarousel" data-slide-to="1"></li>
 				<li data-target="#myCarousel" data-slide-to="2"></li>
 			</ol>
-			<h1>Miguel's Painting & Carpet Cleaning Services</h1>
+			<h1 class="star shadow">Miguel's Painting & Carpet Cleaning Services</h1>
 			<!-- Wrapper for slides -->
 			<div class="carousel-inner">
 				<div class="item active">
 					<div class="carousel-caption">
-						<img src="img/gpsIcon.png">
-						<p>"Miguel's Carpet Cleaning covers up to 40 miles in the Long Beach, Los Angeles area."</p>
+						<img class='star shadow' src="img/gpsIcon.png">
+						<p>Miguel's Carpet Cleaning covers up to 40 miles in the Long Beach, Los Angeles area.</p>
 					</div>
 				</div>
 
 				<div class="item">
 					<div class="carousel-caption">
-						<h3>Chicago</h3>
-						<p>Thank you, Chicago!</p>
+						<img class='star shadow' src="img/moneysaveIcon.png">
+						<p>Quality work that leaves your home and business like new, without leaving your pockets empty.</p>
 					</div>
 				</div>
 
 				<div class="item">
 					<div class="carousel-caption">
-						<h3>New York</h3>
-						<p>We love the Big Apple!</p>
+						<img class='star shadow' src="img/handshakeIcon.png">
+						<p>Lending a helping hand with quality service for my customers. </p>
 					</div>
 				</div>
 			</div>
@@ -75,25 +75,25 @@
 			</a>
 		</div>
 		<div class="about" id="about">
-			<h2>Who Am I?</h2>
+			<h2 class="star">Who Am I?</h2>
 			<p>Hello! My name is Miguel Prieto and I provide professional carpet cleaning and painting services. I am located in the city of Long Beach, and have been running this business for 10 years. I began this business in hopes to give my clients the best painting and carpet cleaning services that any home or business owner would be happy to receive. I love to see the smiles on my clients faces when they see the new look to their house or business. So if you are looking to have a new paint job or a fresh carpet cleaned up and ready for any event you have came to the right place!</p>
 		</div>
 		<div class="services" id="services">
 			<div class="subTitle">
-				<h2>Services</h2>
-				<div class="BnA">
+				<h2 class="animation-element slide-left">Services</h2>
+				<div class="BnA star">
 					<div class="CC">Carpet Cleaning</div>
 					<div class="PS">Painting Services</div>
 				</div>
-				<div class="BnA">
+				<div class="BnA hidden">
 					<div class="before">Before</div>
 					<div class="after">After</div>
 				</div>
 			</div>
 			<div class="container" style="width:100%;">
 				<div class="row">
-				  <div class="col-md-4">
-				    <div class="thumbnail">
+				  <div class="col-md-4 hidden" id="paint">
+				    <div class="thumbnail star">
 				      <a href="/w3images/lights.jpg">
 				        <img src="img/miguelwebsitephotos/painting_before_1.jpg" alt="Before_1" style="width:100%">
 				        <div class="caption">
@@ -102,8 +102,8 @@
 				      </a>
 				    </div>
 				  </div>
-				  <div class="col-md-4">
-				    <div class="thumbnail">
+				  <div class="col-md-4 hidden" id="paint">
+				    <div class="thumbnail star">
 				      <a href="/w3images/nature.jpg">
 				        <img src="img/miguelwebsitephotos/painting_before_2.jpg" alt="Before_2" style="width:100%">
 				        <div class="caption">
@@ -112,8 +112,8 @@
 				      </a>
 				    </div>
 				  </div>
-				  <div class="col-md-4">
-				   <div class="thumbnail">
+				  <div class="col-md-4 hidden" id="paint">
+				   <div class="thumbnail star">
 					 <a href="/w3images/fjords.jpg">
 					   <img src="img/miguelwebsitephotos/painting_before_3.jpg" alt="Before_3" style="width:100%">
 					   <div class="caption">
@@ -122,8 +122,8 @@
 					 </a>
 				   </div>
 				 </div>
-				  <div class="col-md-4">
-				    <div class="thumbnail">
+				  <div class="col-md-4 hidden" id="paint">
+				    <div class="thumbnail star">
 				      <a href="/w3images/fjords.jpg">
 				        <img src="img/miguelwebsitephotos/painting_after_1.jpg" alt="After_1" style="width:100%">
 				        <div class="caption">
@@ -132,8 +132,8 @@
 				      </a>
 				    </div>
 				  </div>
-				 <div class="col-md-4">
-				   <div class="thumbnail">
+				 <div class="col-md-4 hidden" id="paint">
+				   <div class="thumbnail star">
 					 <a href="/w3images/fjords.jpg">
 					   <img src="img/miguelwebsitephotos/painting_after_3.jpg" alt="After_2" style="width:100%">
 					   <div class="caption">
@@ -142,8 +142,8 @@
 					 </a>
 				   </div>
 				 </div>
-				 <div class="col-md-4">
-				   <div class="thumbnail">
+				 <div class="col-md-4 hidden" id="paint">
+				   <div class="thumbnail star">
 					 <a href="/w3images/fjords.jpg">
 					   <img src="img/miguelwebsitephotos/painting_after_4.jpg" alt="After_3" style="width:100%">
 					   <div class="caption">
@@ -152,8 +152,8 @@
 					 </a>
 				   </div>
 				 </div>
-				 <div class="col-md-4">
-				   <div class="thumbnail">
+				 <div class="col-md-4 hidden" id="paint">
+				   <div class="thumbnail star">
 					 <a href="/w3images/fjords.jpg">
 					   <img src="img/miguelwebsitephotos/painting_after_5.jpg" alt="After_4" style="width:100%">
 					   <div class="caption">
@@ -162,8 +162,8 @@
 					 </a>
 				   </div>
 				 </div>
-				 <div class="col-md-4">
-				   <div class="thumbnail">
+				 <div class="col-md-4 hidden" id="paint">
+				   <div class="thumbnail star">
 					 <a href="/w3images/fjords.jpg">
 					   <img src="img/miguelwebsitephotos/painting_after_6.jpg" alt="After_5" style="width:100%">
 					   <div class="caption">
@@ -172,8 +172,8 @@
 					 </a>
 				   </div>
 				 </div>
-				 <div class="col-md-4">
-				   <div class="thumbnail">
+				 <div class="col-md-4 hidden" id="paint">
+				   <div class="thumbnail star">
 					 <a href="/w3images/fjords.jpg">
 					   <img src="img/miguelwebsitephotos/painting_after_7.jpg" alt="After_6" style="width:100%">
 					   <div class="caption">
@@ -186,8 +186,8 @@
 			</div>
 			<div class="container" style="width:100%;">
 				<div class="row">
-					<div class="col-md-4">
-					  <div class="thumbnail">
+					<div class="col-md-4 hidden" id="carpet">
+					  <div class="thumbnail star">
 						<a href="/w3images/fjords.jpg">
 						  <img src="img/miguelwebsitephotos/carpetCleaning_1.jpg" alt="Carpet_Cleaning_1" style="width:100%">
 						  <div class="caption">
@@ -196,8 +196,8 @@
 						</a>
 					  </div>
 					</div>
-					<div class="col-md-4">
-					  <div class="thumbnail">
+					<div class="col-md-4 hidden" id="carpet">
+					  <div class="thumbnail star">
 						<a href="/w3images/fjords.jpg">
 						  <img src="img/miguelwebsitephotos/carpetCleaning_2.jpg" alt="Carpet_Cleaning_2" style="width:100%">
 						  <div class="caption">
@@ -206,8 +206,8 @@
 						</a>
 					  </div>
 					</div>
-					<div class="col-md-4">
-					  <div class="thumbnail">
+					<div class="col-md-4 hidden" id="carpet">
+					  <div class="thumbnail star">
 						<a href="/w3images/fjords.jpg">
 						  <img src="img/miguelwebsitephotos/carpetCleaning_3.jpg" alt="Carpet_Cleaning_3" style="width:100%">
 						  <div class="caption">
@@ -216,8 +216,8 @@
 						</a>
 					  </div>
 					</div>
-					<div class="col-md-4">
-					  <div class="thumbnail">
+					<div class="col-md-4 hidden" id="carpet">
+					  <div class="thumbnail star">
 						<a href="/w3images/fjords.jpg">
 						  <img src="img/miguelwebsitephotos/carpetCleaning_4.jpg" alt="Carpet_Cleaning_4" style="width:100%">
 						  <div class="caption">
@@ -226,8 +226,8 @@
 						</a>
 					  </div>
 					</div>
-					<div class="col-md-4">
-					  <div class="thumbnail">
+					<div class="col-md-4 hidden" id="carpet">
+					  <div class="thumbnail star">
 						<a href="/w3images/fjords.jpg">
 						  <img src="img/miguelwebsitephotos/carpetCleaning_5.jpg" alt="Carpet_Cleaning_5" style="width:100%">
 						  <div class="caption">
@@ -236,8 +236,8 @@
 						</a>
 					  </div>
 					</div>
-					<div class="col-md-4">
-					  <div class="thumbnail">
+					<div class="col-md-4 hidden" id="carpet">
+					  <div class="thumbnail star">
 						<a href="/w3images/fjords.jpg">
 						  <img src="img/miguelwebsitephotos/carpetCleaning_6.jpg" alt="Carpet_Cleaning_6" style="width:100%">
 						  <div class="caption">
@@ -246,8 +246,8 @@
 						</a>
 					  </div>
 					</div>
-					<div class="col-md-4">
-					  <div class="thumbnail">
+					<div class="col-md-4 hidden" id="carpet">
+					  <div class="thumbnail star">
 						<a href="/w3images/fjords.jpg">
 						  <img src="img/miguelwebsitephotos/carpetCleaning_7.jpg" alt="Carpet_Cleaning_7" style="width:100%">
 						  <div class="caption">
@@ -256,8 +256,8 @@
 						</a>
 					  </div>
 					</div>
-					<div class="col-md-4">
-					  <div class="thumbnail">
+					<div class="col-md-4 hidden" id="carpet">
+					  <div class="thumbnail star">
 						<a href="/w3images/fjords.jpg">
 						  <img src="img/miguelwebsitephotos/carpetCleaning_8.jpg" alt="Carpet_Cleaning_8" style="width:100%">
 						  <div class="caption">
@@ -269,12 +269,9 @@
 				</div>
 			</div>
 		</div>
-		<!-- <div class="services"> -->
-
-		<!-- </div> -->
-		<div class="B2T text-right"><a href="#">Back to Services</a></div>
-		<div class="subTitle">
-			<h2>Contact Me</h2>
+		<div class="B2T text-right hidden"><a href="#">Back to Services</a></div>
+		<div class="subTitle contact3">
+			<h2 class="animation-element slide-left">Contact Me</h2>
 		</div>
 		<div class="contact" id="contactMe">
 			<form method="post" action="visualDesign.php#alert" id="contact" class="form-horizontal  text-left">
---
+++ js/script.js
@@ -0,0 +1,103 @@
+$(document).ready(function() {
+/*-------------------------------------
+| Smooth Scroll
+-------------------------------------*/
+        function scrollHome () {
+        	$('html,body').animate({
+                scrollTop: $(".home2").offset().top},
+                'slow');
+        }
+
+        function scrollAbout () {
+        	$('html,body').animate({
+                scrollTop: $("#about").offset().top},'slow');
+        }
+
+        function scrollServices () {
+                $('html,body').animate({
+                scrollTop: $('#services').offset().top},'slow');
+        }
+
+        function scrollContact () {
+                $('html,body').animate({
+                scrollTop: $('.contact3').offset().top},'slow');
+        }
+
+        $(".home").click(scrollHome);
+        $(".about").click(scrollAbout);
+        $(".services").click(scrollServices);
+        $(".contact2").click(scrollContact);
+
+/*-------------------------------------
+| Image Fade When Scrolling
+-------------------------------------*/
+        function showImages(el) {
+                var windowHeight = jQuery( window ).height();
+                $(el).each(function(){
+                    var thisPos = $(this).offset().top;
+
+                    var topOfWindow = $(window).scrollTop();
+                    if (topOfWindow + windowHeight - 200 > thisPos ) {
+                        $(this).addClass("fadeIn");
+                    }
+                });
+            }
+
+            // if the image in the window of browser when the page is loaded, show that image
+            $(document).ready(function(){
+                    showImages('.star');
+            });
+
+            // if the image in the window of browser when scrolling the page, show that image
+            $(window).scroll(function() {
+                    showImages('.star');
+            });
+
+/*-------------------------------------
+| Slide In On View animation
+-------------------------------------*/
+var $animation_elements = $('.animation-element');
+var $window = $(window);
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+      $element.addClass('in-view');
+    } else {
+      $element.removeClass('in-view');
+    }
+  });
+}
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
+
+/*-------------------------------------
+| Show Service On CLick
+-------------------------------------*/
+function displayPainting () {
+  $('#paint').removeClass('hidden');
+  // $('#paint').addClass('show');
+  // $('#carpet').addClass('hidden');
+}
+
+function displayCarpet () {
+  $('#carpet').removeClass('hidden');
+  // $('#carpet').addClass('show');
+  // $('#paint').addClass('hidden');
+}
+
+$('.PS').click(displayPainting);
+$('.CC').click(displayCarpet);
+});
